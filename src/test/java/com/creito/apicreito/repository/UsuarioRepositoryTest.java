f419ac30bb834d6addd873b770870f9d0a80266c
Cristiano Brum <cristianolbrum@gmail.com>
2023-04-26 18:42:03 -0400

    Criação de entidade Usuario e de testes para a mesma

--- src/test/java/com/creito/apicreito/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/creito/apicreito/repository/UsuarioRepositoryTest.java
@@ -1,20 +1,39 @@
 package com.creito.apicreito.repository;
 
 import com.creito.apicreito.entity.Usuario;
-import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+    private static final String EMAIL = "email@teste.com";
     @Autowired
     UsuarioRepository repository;
 
+    @Before
+    public void setUp(){
+        Usuario u = new Usuario();
+        u.setName("Set Up User");
+        u.setPassword("Senha123");
+        u.setEmail(EMAIL);
+
+        repository.save(u);
+    }
+
+    @After
+    public void tearDown(){
+        repository.deleteAll();
+    }
     @Test
     public void testSave(){
         Usuario u = new Usuario();
@@ -23,5 +42,11 @@ public class UsuarioRepositoryTest {
         u.setEmail("teste@teste@gmail.com");
         Usuario response = repository.save(u);
         assertNotNull(response);
+    }
+
+    public void testFindByEmail(){
+        Optional<Usuario> response = repository.findByEmailEquals(EMAIL);
+        assertTrue(response.isPresent());
+        assertEquals(response.get().getEmail(),EMAIL);
     }
 }
