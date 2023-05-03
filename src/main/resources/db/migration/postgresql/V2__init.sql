11b57bc5271ee8b53a11258355893fb187fe908d
Cristiano Brum <cristianolbrum@gmail.com>
2023-05-02 21:13:34 -0400

    Inseridos gerenciador de versionamento flyway ao projeto

---
+++ src/main/resources/db/migration/postgresql/V2__init.sql
@@ -0,0 +1,31 @@
+create table usuarios(
+id serial,
+name varchar(50),
+password varchar,
+email varchar(100),
+primary key (id));
+
+create table carteira(
+id serial,
+name varchar(60),
+value numeric(10,2),
+primary key (id));
+
+create table usuarios_carteira(
+id serial,
+carteira integer,
+usuarios integer,
+primary key(id),
+foreign key(usuarios) references usuarios(id),
+foreign key(carteira) references carteira(id));
+
+create table carteira_itens(
+id serial,
+carteira integer,
+date date,
+type varchar(2),
+description varchar(500),
+value numeric(10,2),
+primary key(id),
+foreign key(carteira) references carteira(id));
+
